a88649912b4f9a4a2f16aa32181d2a2c530a2dde
istiak101 <30789544+istiak101@users.noreply.github.com>
2022-05-26 02:12:18 +0600

    Create cargo-registry.sh

---
+++ cargo-registry.sh
@@ -0,0 +1,17 @@
+dnf upgrade -y
+
+#frontend
+dnf module install nodejs:16
+git clone https://github.com/rust-lang/crates.io.git
+cd crates.io/
+npm install
+
+#backend
+dnf install -y rust
+dnf install -y postgresql-server postgresql-contrib postgresql-devel pkgconfig
+cargo install diesel_cli --no-default-features --features postgres
+createdb cargo_registry
+diesel migration run
+./script/init-local-index.sh
+cargo run --bin background-worker
+#node http://localhost:4200!
